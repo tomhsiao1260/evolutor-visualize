16035248dc1cd140d915b4e155854f1bc627c6ed
Hsiao Yao <tomtogo1260@gmail.com>
2025-10-25 06:12:23 +0800

    a better result

--- m.py
+++ m.py
@@ -2,42 +2,44 @@ import sys
 import cmap
 import numpy as np
 import matplotlib.pyplot as plt
-from test import create_angle_array
 from test import merge_rectangle
 from test import merge_bridge
 from test import merge_chunk
 from test import normalize
 
-def merge(chunk_left, chunk_right, chunk_avg):
+def merge(chunk_left, chunk_right):
     l = chunk_left.copy()
     r = chunk_right.copy()
-    a = chunk_avg.copy()
-
-    h0, w0 = chunk_left.shape
-    chunk = np.zeros_like(chunk_avg)
     d = l[:,-1:] - r[:,:1]
 
+    shift = .5
+    h0, w0 = chunk_left.shape
+    chunk = np.zeros((h0, 2*w0), dtype=l.dtype)
+
     x, y, w, h = 0, 0, w0, h0
+    chunk[y:y+h, x:x+w]  = normalize(-2*l + d)
+    chunk[y:y+h, x:x+w] *= normalize(-1*l + 0) + shift
+    chunk[y:y+h, x:x+w] /= normalize(-1*l + d) + shift
+    chunk[y:y+h, x:x+w]  = normalize(-chunk[y:y+h, x:x+w])
     # chunk[y:y+h, x:x+w] = l
-    chunk[y:y+h, x:x+w] = normalize(-a[y:y+h, x:x+w].copy())
-    chunk[y:y+h, x:x+w] *= normalize(-a[y:y+h, x:x+w].copy()) + 1.0
-    chunk[y:y+h, x:x+w] /= normalize(-l.copy() + d) + 1.0
-    chunk[y:y+h, x:x+w] = normalize(-chunk[y:y+h, x:x+w])
     x, y, w, h = w0, 0, w0, h0
+    chunk[y:y+h, x:x+w]  = normalize(2*r + d)
+    chunk[y:y+h, x:x+w] *= normalize(1*r + 0) + shift
+    chunk[y:y+h, x:x+w] /= normalize(1*r + d) + shift
+    chunk[y:y+h, x:x+w]  = normalize(chunk[y:y+h, x:x+w])
     # chunk[y:y+h, x:x+w] = r
-    chunk[y:y+h, x:x+w] = normalize(a[y:y+h, x:x+w].copy())
-    chunk[y:y+h, x:x+w] *= normalize(a[y:y+h, x:x+w].copy()) + 1.0
-    chunk[y:y+h, x:x+w] /= normalize(r.copy() + d) + 1.0
-    chunk[y:y+h, x:x+w] = normalize(chunk[y:y+h, x:x+w])
     return align(chunk)
 
 def align(chunk):
     h0, w0 = chunk.shape
-    d = (chunk[:, w0//2-1:w0//2] - chunk[:, w0//2:w0//2+1]) / 2
+    s = w0//2
+    dl = chunk[:, s-2:s-1] - chunk[:, s-1:s+0]
+    dm = chunk[:, s-1:s+0] - chunk[:, s+0:s+1]
+    dr = chunk[:, s+0:s+1] - chunk[:, s+1:s+2]
     x, y, w, h = 0, 0, w0//2, h0
-    chunk[y:y+h, x:x+w] -= d
+    chunk[y:y+h, x:x+w] -= (dm - dl) / 2
     x, y, w, h = w0//2, 0, w0//2, h0
-    chunk[y:y+h, x:x+w] += d
+    chunk[y:y+h, x:x+w] += (dm - dr) / 2
     return normalize(chunk)
 
 def main():
@@ -50,7 +52,6 @@ def main():
     xs, ys = np.meshgrid(np.linspace(0, 1, w0), np.linspace(0, 1, h0))
 
     x, y, w, h = 0, 0, w0, h0
-    # angle_array = create_angle_array(h0)[::-1,::-1]
     angle_array = xs - ys
     # angle_array = xs + ys
     angle_array -= np.min(angle_array)
@@ -58,60 +59,27 @@ def main():
     imageA[y:y+h, x:x+w] = angle_array
 
     x, y, w, h = w0, 0, w0, h0
-    # angle_array = create_angle_array(h0)[:,::-1].T
     angle_array = xs + ys
     angle_array -= np.min(angle_array)
     angle_array /= np.max(angle_array)
     imageA[y:y+h, x:x+w] = angle_array
 
     x, y, w, h = w0//2, 0, w0, h0
-    # imageB[y:y+h, x:x+w] = angle_array
     imageB[y:y+h, x:x+w] = np.linspace(0, 1, w)
 
     x, y, w, h = 0, 0, w0, h0
-    a = imageA[:, w0-1:w0].copy()
-    b = imageA[:, w0:w0+1].copy()
-    diff = a-b
-    imageC[y:y+h, x:x+w] = normalize(-(2.0*imageA[y:y+h, x:x+w].copy() - diff))
-    imageC[y:y+h, x:x+w] *= normalize(-(2.0*imageA[y:y+h, x:x+w].copy() - diff)) + 1.0
-    imageC[y:y+h, x:x+w] /= normalize(-(1.0*imageA[y:y+h, x:x+w].copy() - diff)) + 1.0
-    imageC[y:y+h, x:x+w] = normalize(-imageC[y:y+h, x:x+w])
+    chunk_left = imageA[y:y+h, x:x+w].copy()
     x, y, w, h = w0, 0, w0, h0
-    imageC[y:y+h, x:x+w] = normalize(2.0*imageA[y:y+h, x:x+w].copy() + diff)
-    imageC[y:y+h, x:x+w] *= normalize(2.0*imageA[y:y+h, x:x+w].copy() + diff) + 1.0
-    imageC[y:y+h, x:x+w] /= normalize(1.0*imageA[y:y+h, x:x+w].copy() + diff) + 1.0
-    imageC[y:y+h, x:x+w] = normalize(imageC[y:y+h, x:x+w])
-
-    x, y, w, h = 0, 0, w0, h0
-    a = imageC[:, w0-1:w0].copy()
-    b = imageC[:, w0:w0+1].copy()
-    diff = (a-b)/2
-    imageC[y:y+h, x:x+w] = imageC[y:y+h, x:x+w].copy() - diff
-    x, y, w, h = w0, 0, w0, h0
-    imageC[y:y+h, x:x+w] = imageC[y:y+h, x:x+w].copy() + diff
+    chunk_right = imageA[y:y+h, x:x+w].copy()
     x, y, w, h = 0, 0, 2*w0, h0
-    imageC[y:y+h, x:x+w] = normalize(imageC[y:y+h, x:x+w])
+    imageC[y:y+h, x:x+w] = merge(chunk_left, chunk_right)
 
-    x, y, w, h = w0//2, h0//2, w0//2, h0//2
+    x, y, w, h = w0//2, 0, w0//2, h0
     chunk_left = imageA[y:y+h, x:x+w].copy()
-    x, y, w, h = w0, h0//2, w0//2, h0//2
+    x, y, w, h = w0, 0, w0//2, h0
     chunk_right = imageA[y:y+h, x:x+w].copy()
-    x, y, w, h = w0//2, h0//2, w0, h0//2
-    chunk_avg = imageB[y:y+h, x:x+w].copy()
-    x, y, w, h = w0//2, h0//2, w0, h0//2
-    imageD[y:y+h, x:x+w] = merge(chunk_left, chunk_right, chunk_avg)
-
-    x, y, w, h = w0//2, 0, w0//2, h0//2
-    chunk_left = imageA[y:y+h, x:x+w].copy()
-    x, y, w, h = w0, 0, w0//2, h0//2
-    chunk_right = imageA[y:y+h, x:x+w].copy()
-    x, y, w, h = w0//2, 0, w0, h0//2
-    chunk_avg = imageB[y:y+h, x:x+w].copy()
-    x, y, w, h = w0//2, 0, w0, h0//2
-    imageD[y:y+h, x:x+w] = merge(chunk_left, chunk_right, chunk_avg)
-
     x, y, w, h = w0//2, 0, w0, h0
-    # imageD[y:y+h, x:x+w] = align(imageD[y:y+h, x:x+w].T).T
+    imageD[y:y+h, x:x+w] = merge(chunk_left, chunk_right)
 
     row_num, col_num = 2, 2
     fig, axes = plt.subplots(row_num, col_num, figsize=(9, 5))
@@ -131,7 +99,6 @@ def main():
     axes[1, 1].set_title('D')
 
     plt.show()
-
 
 if __name__ == '__main__':
     sys.exit(main())
