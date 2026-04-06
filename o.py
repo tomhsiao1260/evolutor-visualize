cb40b46ece2b6903005d2925ff7e853825b2daf2
Hsiao Yao <tomtogo1260@gmail.com>
2026-04-06 23:09:36 +0800

    think about addition rule

--- o.py
+++ o.py
@@ -3,38 +3,79 @@ import cmap
 import numpy as np
 import matplotlib.pyplot as plt
 
-s, f = 100, 10
-h0, w0 = s, s
-h1, w1 = s//f, s//f
+h0, w0 = 100, 100
+x0, y0 = np.meshgrid(np.linspace(0, 1, w0), np.linspace(0, 1, h0))
 
-xs, ys = np.meshgrid(np.linspace(0, 1, w0), np.linspace(0, 1, h0))
-xs -= 0.5
-ys -= 0.5
+h1, w1 = h0//10, w0//10
+x1, y1 = np.meshgrid(np.arange(w1), np.arange(h1))
 
-imageA = np.sqrt(xs*xs + ys*ys)
-imageA -= np.min(imageA)
-imageA /= np.max(imageA)
-
-padA = np.pad(imageA, pad_width=1, mode='edge')
-dx = padA[1:-1,2:] - padA[1:-1,1:-1]
-dy = padA[2:,1:-1] - padA[1:-1,1:-1]
-
-dx = cv2.resize(dx, (h1, w1), interpolation=cv2.INTER_LINEAR)
-dy = cv2.resize(dy, (h1, w1), interpolation=cv2.INTER_LINEAR)
-x1, y1 = np.meshgrid(np.linspace(0, 1, w1), np.linspace(0, 1, h1))
-
-row_num, col_num = 2, 2
-fig, axes = plt.subplots(row_num, col_num, figsize=(5, 5))
+row_num, col_num = 2, 6
+fig, axes = plt.subplots(row_num, col_num, figsize=(15, 5))
 colormap = cmap.Colormap("tab20", interpolation="nearest")
 for ax in axes.flat: ax.axis('off')
 
-axes[0, 0].imshow(colormap(imageA), aspect='equal')
-axes[0, 0].set_title('F')
+# A = np.sqrt(x0*x0 + y0*y0)
+A = x0 - y0
+A -= np.min(A)
+A /= np.max(A)
 
-axes[0, 1].quiver(x1*w1, y1*h1, dx, dy, angles='xy')
-axes[0, 1].set_xlim(0, w1)
-axes[0, 1].set_ylim(h1, 0)
-axes[0, 1].grid()
-axes[0, 1].set_title('dF')
+dx = A[1:-1,2:] - A[1:-1,1:-1]
+dy = A[2:,1:-1] - A[1:-1,1:-1]
+dx = cv2.resize(dx, (h1, w1), interpolation=cv2.INTER_LINEAR)
+dy = cv2.resize(dy, (h1, w1), interpolation=cv2.INTER_LINEAR)
+
+i, j = 0, 0
+axes[i, j].imshow(colormap(A), aspect='equal')
+axes[i, j].set_title('F')
+
+i, j = 0, 1
+axes[i, j].quiver(x1, y1, dx, dy, angles='xy')
+axes[i, j].set_aspect('equal')
+axes[i, j].set_xlim(0, w1)
+axes[i, j].set_ylim(h1, 0)
+axes[i, j].grid()
+axes[i, j].set_title('dF')
+
+B = y0 * x0
+B -= np.min(B)
+B /= np.max(B)
+
+dx = B[1:-1,2:] - B[1:-1,1:-1]
+dy = B[2:,1:-1] - B[1:-1,1:-1]
+dx = cv2.resize(dx, (h1, w1), interpolation=cv2.INTER_LINEAR)
+dy = cv2.resize(dy, (h1, w1), interpolation=cv2.INTER_LINEAR)
+
+i, j = 0, 2
+axes[i, j].imshow(colormap(B), aspect='equal')
+axes[i, j].set_title('F')
+
+i, j = 0, 3
+axes[i, j].quiver(x1, y1, dx, dy, angles='xy')
+axes[i, j].set_aspect('equal')
+axes[i, j].set_xlim(0, w1)
+axes[i, j].set_ylim(h1, 0)
+axes[i, j].grid()
+axes[i, j].set_title('dF')
+
+C = x0-y0 + y0*x0
+C -= np.min(C)
+C /= np.max(C)
+
+dx = C[1:-1,2:] - C[1:-1,1:-1]
+dy = C[2:,1:-1] - C[1:-1,1:-1]
+dx = cv2.resize(dx, (h1, w1), interpolation=cv2.INTER_LINEAR)
+dy = cv2.resize(dy, (h1, w1), interpolation=cv2.INTER_LINEAR)
+
+i, j = 0, 4
+axes[i, j].imshow(colormap(C), aspect='equal')
+axes[i, j].set_title('F')
+
+i, j = 0, 5
+axes[i, j].quiver(x1, y1, dx, dy, angles='xy')
+axes[i, j].set_aspect('equal')
+axes[i, j].set_xlim(0, w1)
+axes[i, j].set_ylim(h1, 0)
+axes[i, j].grid()
+axes[i, j].set_title('dF')
 
 plt.show()
